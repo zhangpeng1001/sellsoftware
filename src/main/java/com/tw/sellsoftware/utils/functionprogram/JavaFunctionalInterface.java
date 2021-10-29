a8d83c138f8924ab1ee31559b6439093fad06faa
peng.zhang1 <brightfuture1001@163.com>
2021-10-29 10:58:26 +0800

    Study functional program through The Function class.

--- src/main/java/com/tw/sellsoftware/utils/functionprogram/JavaFunctionalInterface.java
+++ src/main/java/com/tw/sellsoftware/utils/functionprogram/JavaFunctionalInterface.java
@@ -1,16 +1,27 @@
 package com.tw.sellsoftware.utils.functionprogram;
 
 import java.util.function.Consumer;
+import java.util.function.Function;
 
 public class JavaFunctionalInterface {
 
     public static void main(String[] args) {
         consumerTest();
+        functionTest();
     }
+
     public static void consumerTest() {
         Consumer consumer1 = System.out::println;
-        Consumer consumer2 = o->System.out.println("consumer2:"+o);
+        Consumer consumer2 = o -> System.out.println("consumer2:" + o);
         consumer1.andThen(consumer2).accept("Today is friday!");
         consumer1.andThen(consumer1).andThen(consumer1).andThen(consumer2).accept("Tomorrow is Saturday");
+    }
+
+    public static void functionTest() {
+        Function<Integer, Integer> function1 = num -> ++num;
+        Function<Integer, Integer> function2 = num -> num * 2;
+        System.out.println(function1.compose(function2).apply(1));
+        System.out.println(function1.andThen(function2).apply(2));
+        System.out.println(Function.identity().apply(3));
     }
 }
